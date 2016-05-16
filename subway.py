ff134f4c92397a18bf8e5028f063f0c8e520b4cc
KhadeejaDin <khadeejadin@gmail.com>
2016-05-16 14:16:46 -0400

    updated subway.py

--- subway.py
+++ subway.py
@@ -29,7 +29,7 @@ def mapToZone(parts):
     import pyproj #only to conver long lat to x y in feets
     import shapely.geometry as geom #only to convert (x,y) to Point (x y)
     proj = pyproj.Proj(init="epsg:2263", preserve_units=True)
-    index, zones = indexZones('SubwayStations.geojson')
+    index, zones = indexZones(sys.argv[1])
     #return zones.geometry
     for line in parts: #convert long lat to x y
         fields = line.strip().split(',')
@@ -47,7 +47,7 @@ if __name__=='__main__':
 
     sc = pyspark.SparkContext()
 
-    lines = sc.textFile(','.join(sys.argv[1:-1]))
+    lines = sc.textFile(','.join(sys.argv[2:-1]))
     trips = lines.filter(lambda x: not x.startswith('Unique Key') and x != '')
 
     output = trips.mapPartitions(mapToZone).reduceByKey(lambda a, b: a+b)
